3151b34b5200d49821f58f54f3e4e05867e368cc
Real man born on semtember <hieunam03@gmail.com>
2018-05-18 12:38:06 +0700

    Update and rename lineage.mk to dot.mk

--- dot.mk
+++ dot.mk
@@ -1,12 +1,12 @@
 
 # Inherit from common
-$(call inherit-product, device/samsung/gprimelte-common/lineage.mk)
+$(call inherit-product, device/samsung/gprimelte-common/dot.mk)
 
 $(call inherit-product, device/samsung/gprimelte/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gprimelte
-PRODUCT_NAME := lineage_gprimelte
+PRODUCT_NAME := dot_gprimelte
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G530W
 PRODUCT_MANUFACTURER := samsung
